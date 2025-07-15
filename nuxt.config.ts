e7fe4ef8465e919c139b4ae18591beae254c5f93
Jose Barrientos <pro@portaldj.cl>
2025-07-15 10:44:27 -0400

    Removes Cloudflare Nitro preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  nitro: {
-    preset: 'cloudflare'
-  },
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
