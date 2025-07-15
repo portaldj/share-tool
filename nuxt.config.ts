5c7a7dc03cf73841a2118e1162ec706dd6d8ab19
Jose Barrientos <pro@portaldj.cl>
2025-07-15 10:20:39 -0400

    Configures Nitro preset for Cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  nitro: {
+    preset: 'cloudflare'
+  },
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
